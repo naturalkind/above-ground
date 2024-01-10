bd8f4c30780e9ba4a4dc3b88a5515598a4e2fe28
naturalkind <adoo.ssodl@gmail.com>
2024-01-11 00:13:33 +0300

    socket обмен данными

---
+++ web-UI/client.py
@@ -0,0 +1,71 @@
+import cv2
+import socket
+import pickle
+import struct
+import tracker_lib
+
+lib_start = tracker_lib.TrackerLib()
+# Создание сокета
+client_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+host_ip = '192.168.1.123'  # Адрес сервера
+port = 9999
+client_socket.connect((host_ip, port))
+data = b""
+payload_size = struct.calcsize("Q")
+
+while True:
+    timer = cv2.getTickCount()
+    
+    # получение данных
+    while len(data) < payload_size:
+        packet = client_socket.recv(4*1024)
+        if not packet: break
+        data += packet
+    packed_msg_size = data[:payload_size]
+    data = data[payload_size:]
+    msg_size = struct.unpack("Q",packed_msg_size)[0]
+    
+    while len(data) < msg_size:
+        data += client_socket.recv(4*1024)
+    frame_data = data[:msg_size]
+    data = data[msg_size:]
+    img, init_tracker = pickle.loads(frame_data)
+     
+    if init_tracker:
+        lib_start.init_switch = False
+        lib_start.state = 0
+    # выделение мышкой
+    bbox = lib_start.process_img_client(img)
+                        
+    # Если начальные и конечные координаты прямоугольника определены
+    if lib_start.start_x != -1 and lib_start.end_x != -1:
+        if lib_start.state != 0:
+            # Рисование прямоугольника на изображении
+            cv2.rectangle(img, (lib_start.start_x, lib_start.start_y), (lib_start.end_x, lib_start.end_y), (0, 255, 0), 2)
+    
+    # визуализация
+    cv2.imshow("win", img)
+    if cv2.waitKey(1) & 0xff == ord('q'):
+        client_socket.close()
+        break
+          
+    # Отправка обработанного изображения серверу
+    #img = cv2.flip(img, 1)
+    a = pickle.dumps((bbox,
+                      lib_start.state,
+                      lib_start.init_switch))
+    message = struct.pack("Q", len(a)) + a
+    client_socket.sendall(message)
+        
+        
+    fps = cv2.getTickFrequency()/(cv2.getTickCount()-timer)    
+    #print (img.shape, fps) 
+     
+cap.release()
+cv2.destroyAllWindows()
+
+client_socket.close()
+
+
+
+
---
+++ web-UI/server.py
@@ -0,0 +1,77 @@
+import cv2
+import socket
+import pickle
+import struct
+import tracker_lib
+
+lib_start = tracker_lib.TrackerLib()
+# Создание сокета
+server_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+host_name = socket.gethostname()
+host_ip = '192.168.1.123'#socket.gethostbyname(host_name)
+print('Хост IP:', host_ip)
+port = 9999
+socket_address = (host_ip, port)
+
+# Привязка сокета
+server_socket.bind(socket_address)
+
+# Ожидание подключения клиента
+server_socket.listen(5)
+print("Ожидание подключения клиента...")
+cap = cv2.VideoCapture(1)
+
+payload_size = struct.calcsize("Q")
+data = b""
+init_tracker = False
+while True:
+    client_socket,addr = server_socket.accept()
+    print('Получено соединение от:', addr)
+    if client_socket:
+        while(cap.isOpened()):
+            success, _img = cap.read()
+            
+            if init_tracker == False:
+                # отправка данных
+                a = pickle.dumps([_img, init_tracker])
+                message = struct.pack("Q", len(a)) + a
+                client_socket.sendall(message)
+            
+            
+            # получение данных
+            while len(data) < payload_size:
+                packet = client_socket.recv(4*1024)
+                if not packet: break
+                data += packet
+            packed_msg_size = data[:payload_size]
+            data = data[payload_size:]
+            msg_size = struct.unpack("Q",packed_msg_size)[0]
+            
+            while len(data) < msg_size:
+                data += client_socket.recv(4*1024)
+            frame_data = data[:msg_size]
+            data = data[msg_size:]
+            bbox, state, init_switch = pickle.loads(frame_data)  
+            
+            if state > 1 and init_switch is True:
+                if init_tracker == False:
+                    lib_start.init_tracker(_img, bbox)
+                init_tracker = True
+            img = lib_start.process_img_server(_img)     
+            
+            
+            if init_tracker == True:
+                a = pickle.dumps([img, init_tracker])
+                message = struct.pack("Q", len(a)) + a
+                client_socket.sendall(message)
+            # визуализация
+            cv2.imshow("win", img)
+            if cv2.waitKey(1) & 0xff == ord('q'):
+                client_socket.close()
+                break 
+                
+                
+        cap.release()
+        cv2.destroyAllWindows()
+
+
